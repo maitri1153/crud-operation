8631d1b51444fac9a28383e87d7ae7be894c653c
maitri <maitripatel9963@gmail.com>
2025-03-27 12:09:25 +0530

    create constant folder

--- src/main/java/com/ignek/crud/connection/DBConnection.java
+++ src/main/java/com/ignek/crud/connection/DBConnection.java
@@ -4,18 +4,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import com.ignek.crud.constant.Constant;
+
 public class DBConnection {
 	public static Connection initializeDatabase() throws SQLException, ClassNotFoundException {
-		// Initialize all the information regarding Database Connection
-		String dbDriver = "com.mysql.cj.jdbc.Driver";
-		String dbURL = "jdbc:mysql://localhost:3306/";
-		// Database name to access
-		String dbName = "mysql_database";
-		String dbUsername = "root";
-		String dbPassword = "ignek@12345";
-
-		Class.forName(dbDriver);
-		Connection con = DriverManager.getConnection(dbURL + dbName, dbUsername, dbPassword);
-		return con;
+		
+		Class.forName(Constant.DRIVER);
+		Connection connection = DriverManager.getConnection(Constant.PATH,Constant.USERID,Constant.PASSWORD);
+		return connection;
 	}
 }
