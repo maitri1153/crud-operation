780e83b2c27e98e383d2e98e03ba8c94fffd8824
maitri <maitripatel9963@gmail.com>
2025-03-28 16:32:04 +0530

    Added catch statement into DBConnection

--- src/main/java/com/ignek/crud/connection/DBConnection.java
+++ src/main/java/com/ignek/crud/connection/DBConnection.java
@@ -3,6 +3,7 @@ package com.ignek.crud.connection;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
 import com.ignek.crud.constant.EmployeeConstant;
 
 public class DBConnection {
@@ -10,11 +11,10 @@ public class DBConnection {
 		Connection connection = null;
 		try {
 			Class.forName(EmployeeConstant.DRIVER);
-			connection = DriverManager.getConnection(EmployeeConstant.PATH, EmployeeConstant.USER_ID, 
-					EmployeeConstant.PASSWORD);
+			connection = DriverManager.getConnection(EmployeeConstant.PATH, EmployeeConstant.USERID, EmployeeConstant.PASSWORD);
 			return connection;
 		} catch (Exception e) {
-			e.printStackTrace();
+
 		}
 		return connection;
 	}
