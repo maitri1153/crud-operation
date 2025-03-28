cd4444867d66c3d31796c7e3615c8082ab95d505
Maitri Patel <maitripatel9963@gmail.com>
2025-03-28 14:59:08 +0530

    Delete src/main/java/com/ignek/crud/constant/Constant.java

--- src/main/java/com/ignek/crud/constant/Constant.java
+++
@@ -1,9 +0,0 @@
-package com.ignek.crud.constant;
-
-public class Constant {
-
-	public static String PATH = "jdbc:mysql://localhost:3306/mysql_database"; 
-	public static String USERID = "root";
-	public static String PASSWORD = "ignek@12345";
-	public static String DRIVER = "com.mysql.cj.jdbc.Driver";
-}
