ad26aa76c28c00389219d9cadb6dc0a29ad5775e
maitri <maitripatel9963@gmail.com>
2025-03-31 10:39:38 +0530

    Added catch statement

--- src/main/java/com/ignek/crud/connection/DBConnection.java
+++ src/main/java/com/ignek/crud/connection/DBConnection.java
@@ -3,18 +3,18 @@ package com.ignek.crud.connection;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
 import com.ignek.crud.constant.EmployeeConstant;
 
 public class DBConnection {
-	public static Connection initializeDatabase() throws SQLException, ClassNotFoundException {
+	public static Connection initializeDatabase()throws SQLException, ClassNotFoundException {
 		Connection connection = null;
 		try {
 			Class.forName(EmployeeConstant.DRIVER);
-			connection = DriverManager.getConnection(EmployeeConstant.PATH, EmployeeConstant.USERID, EmployeeConstant.PASSWORD);
+			connection = DriverManager.getConnection(EmployeeConstant.PATH, EmployeeConstant.USER_ID, 
+					EmployeeConstant.PASSWORD);
 			return connection;
 		} catch (Exception e) {
-
+			e.printStackTrace();
 		}
 		return connection;
 	}
