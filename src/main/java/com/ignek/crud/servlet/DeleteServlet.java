5cebcb0b6ef51b4513977c96da6611e339eccb5d
maitri <maitripatel9963@gmail.com>
2025-03-31 12:22:21 +0530

    Refactored DeleteServlet

--- src/main/java/com/ignek/crud/servlet/DeleteServlet.java
+++ src/main/java/com/ignek/crud/servlet/DeleteServlet.java
@@ -1,16 +1,15 @@
 package com.ignek.crud.servlet;
 
+import java.io.IOException;
+
+import com.ignek.crud.constant.EmployeeConstant;
+import com.ignek.crud.dao.EmployeeDAO;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-
-import com.ignek.crud.connection.DBConnection;
 
 @WebServlet("/DeleteServlet")
 public class DeleteServlet extends HttpServlet {
@@ -18,23 +17,18 @@ public class DeleteServlet extends HttpServlet {
 
 	public DeleteServlet() {
 		super();
-
 	}
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
-		 response.setContentType("text/html");
-		 int emp =Integer.parseInt(request.getParameter("id"));
-		 
-		 try {
-			 Connection con = DBConnection.initializeDatabase();
-			 PreparedStatement pts;
-			 pts = con.prepareStatement("delete from Employee where emp_id =?");
-			 pts.setInt(1,emp);
-			 pts.executeUpdate();
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			int id = Integer.parseInt(request.getParameter(EmployeeConstant.Id));
+			EmployeeDAO.deleteEmployee(id);
+			RequestDispatcher dispatcher = request.getRequestDispatcher("InsertServlet");
+			dispatcher.forward(request, response);
 		} catch (Exception e) {
-			System.out.println(e);
+			e.printStackTrace();
 		}
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/registerform.jsp");
-		dispatcher.forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
