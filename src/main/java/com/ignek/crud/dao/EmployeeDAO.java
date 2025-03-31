1c20d62e91e3d9e4cb63554e621794ae5cd99a5e
maitri <maitripatel9963@gmail.com>
2025-03-31 12:19:01 +0530

    Refactored EmployeeDAO file

--- src/main/java/com/ignek/crud/dao/EmployeeDAO.java
+++ src/main/java/com/ignek/crud/dao/EmployeeDAO.java
@@ -6,7 +6,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
 import com.ignek.crud.connection.DBConnection;
 import com.ignek.crud.constant.EmployeeConstant;
 import com.ignek.crud.dto.Employee;
