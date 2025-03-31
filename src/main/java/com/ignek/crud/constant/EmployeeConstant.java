ee09a318a72991e2c1457003a39f0221bd1c1be9
maitri <maitripatel9963@gmail.com>
2025-03-31 16:43:53 +0530

    Refactored EmployeeConstant file

--- src/main/java/com/ignek/crud/constant/EmployeeConstant.java
+++ src/main/java/com/ignek/crud/constant/EmployeeConstant.java
@@ -8,6 +8,7 @@ public class EmployeeConstant {
 	public static String INSERT ="INSERT into Employee(name,email,gender,dob,hobby) VALUES (?,?,?,?,?);";
 	public static String DELETE = "delete from Employee where emp_id=?;";
 	public static String SELECT_USERS = "select * from Employee";
+	public static String SELECT_USER_BY_ID = "select name, email, gender, dob, hobby from Employee where emp_id=?";
 	public static String UPDATE = "update Employee set name=?, email=?,gender=?, dob=?,hobby=? where emp_id=?";
 	public static String Id = "id";
 	public static String ID ="emp_id";
