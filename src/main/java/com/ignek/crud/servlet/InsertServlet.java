c9839ad5acbe98261d7e7a32626211c7f9dd5ab0
maitri <maitripatel9963@gmail.com>
2025-04-02 16:08:08 +0530

    Modified InsertServlet

--- src/main/java/com/ignek/crud/servlet/InsertServlet.java
+++ src/main/java/com/ignek/crud/servlet/InsertServlet.java
@@ -1,20 +1,17 @@
 package com.ignek.crud.servlet;
 
 import java.io.IOException;
-import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
 import org.apache.catalina.tribes.util.Arrays;
 import com.ignek.crud.constant.EmployeeConstant;
 import com.ignek.crud.dao.EmployeeDAO;
 import com.ignek.crud.dto.Employee;
-import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/InsertServlet")
 public class InsertServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -24,27 +21,37 @@ public class InsertServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		List<Employee> employeelist = EmployeeDAO.selectAllEmployees();
-		request.setAttribute("employeelist", employeelist);
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/registerform.jsp");
-		dispatcher.forward(request, response);
+		List<Employee> employeelist = EmployeeDAO.getAllEmployees();
+		request.setAttribute(EmployeeConstant.EMPLOYEE_LIST, employeelist);
+		request.getRequestDispatcher("register.jsp").forward(request, response);
 	}
 
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+			throws ServletException, IOException, NumberFormatException{
 		try {
+			Optional<String> id = Optional.of(request.getParameter(EmployeeConstant.ID));
 			String name = request.getParameter(EmployeeConstant.FULL_NAME);
 			String email = request.getParameter(EmployeeConstant.EMAIL);
 			String gender = request.getParameter(EmployeeConstant.GENDER);
 			String dob = request.getParameter(EmployeeConstant.DOB);
 			String[] hobbylist = request.getParameterValues(EmployeeConstant.HOBBY);
 			String hobby = Arrays.toString(hobbylist);
-			Employee employee = new Employee(name, email, gender, dob, hobby);
-			EmployeeDAO.insertEmployee(employee);
-		} catch (SQLException e) {
+			System.out.println(id);
+			if(id.isEmpty())
+			{
+				Employee employee = new Employee(name, email, gender, dob, hobby);
+				EmployeeDAO.insertEmployee(employee);
+			}
+			else
+			{
+				int emp_id = Integer.parseInt(request.getParameter(EmployeeConstant.ID));
+				Employee employee = new Employee(emp_id,name, email, gender, dob, hobby);
+				EmployeeDAO.updateEmployee(employee);
+			}	
+		}catch (Exception e) {
 			e.printStackTrace();
 		}
 		doGet(request, response);
 	}
-
 }
